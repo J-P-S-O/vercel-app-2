e4971b3b0691618a746dfd6ff83b4d396aa5d712
J-p-s-o <jpso.guerreiro@gmail.com>
2021-02-03 10:09:49 -0300

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 
 export default function Home(req, res) {
   return (
+<div>
     <h1>Welcome to Quartzum!</h1>
     <script>setTimeout(() => {
       window.location = "https://github.com/j-p-s-o/Quartzum"
     },2000);</script>
+</div>
   )
 }
